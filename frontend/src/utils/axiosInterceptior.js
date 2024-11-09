09a21bf2a79b52755ef5fc61d2cb6ff917ea80e1
adityadixit07 <adityadixit9660@gmail.com>
2024-11-09 15:56:33 +0530

    updated

--- frontend/src/utils/axiosInterceptior.js
+++ frontend/src/utils/axiosInterceptior.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const axiosInterceptor = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://digiaccel-todoapp-backend.onrender.com/api",
   // baseURL: "/api",
 });
 
